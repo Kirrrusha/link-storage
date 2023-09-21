74ec9a21ef113cbc059b8433a54813ad577a0357
kirrrrusha <kirilllebedenko@gmail.com>
2023-09-21 21:39:29 +0300

    add cookie

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import * as cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser';
 import { HttpExceptionFilter } from './exceptions/http-exception.filter';
 // import { HttpErrorFilter } from './exceptions/http-error-filter.filter';
 // import { GlobalExceptionFilter } from 'exceptions/http-entity-exception.filter';
@@ -12,12 +12,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: false });
 
   app.enableCors({ credentials: true, origin: true });
+  app.use(cookieParser());
+
 
   app.setGlobalPrefix('api/v2');
 
   const config = new DocumentBuilder()
     .setTitle('Etonemusor API')
-    .setVersion('1.0')
+    .setVersion('3.0')
     .addBearerAuth()
     .build();
 
@@ -33,7 +35,7 @@ async function bootstrap() {
   app.useGlobalFilters(new HttpExceptionFilter());
 
   app.getHttpAdapter().getInstance().disable('x-powered-by');
-  app.use(cookieParser());
+
   await app.listen(port);
   Logger.log(`🚀 Application is running on: http://localhost:${port}`);
 }

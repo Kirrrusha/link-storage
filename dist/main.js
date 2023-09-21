680d83d62d7314fdc2d29a66c25554ea560fb12d
kirrrrusha <kirilllebedenko@gmail.com>
2023-09-21 21:39:29 +0300

    add cookie

--- dist/main.js
+++ dist/main.js
@@ -1,37 +1,22 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const common_1 = require("@nestjs/common");
 const swagger_1 = require("@nestjs/swagger");
 const app_module_1 = require("./app.module");
-const cookieParser = __importStar(require("cookie-parser"));
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const http_exception_filter_1 = require("./exceptions/http-exception.filter");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule, { cors: false });
     app.enableCors({ credentials: true, origin: true });
+    app.use(cookie_parser_1.default());
     app.setGlobalPrefix('api/v2');
     const config = new swagger_1.DocumentBuilder()
         .setTitle('Etonemusor API')
-        .setVersion('1.0')
+        .setVersion('3.0')
         .addBearerAuth()
         .build();
     const document = swagger_1.SwaggerModule.createDocument(app, config);
@@ -43,7 +28,6 @@ async function bootstrap() {
     const port = process.env.PORT || 3000;
     app.useGlobalFilters(new http_exception_filter_1.HttpExceptionFilter());
     app.getHttpAdapter().getInstance().disable('x-powered-by');
-    app.use(cookieParser());
     await app.listen(port);
     common_1.Logger.log(`🚀 Application is running on: http://localhost:${port}`);
 }
